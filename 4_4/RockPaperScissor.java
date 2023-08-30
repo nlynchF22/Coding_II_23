abb2d291c2f8a7ddb37556acfd6254ad1f1cf5d3
nlynchF22 <120128738+nlynchF22@users.noreply.github.com>
2023-08-30 16:44:45 +0000

    commiting

--- 4_4/RockPaperScissor.java
+++ 4_4/RockPaperScissor.java
@@ -1,8 +1,16 @@
-
+import java.util.Random;
+import java.util.Scanner;
 public class RockPaperScissor {
 
     public static void main(String[] args) {
-        
+        Scanner numberScanner = new Scanner(System.in);
+        System.out.print("Enter a number between 1 and 3: ");
+        int userNum = numberScanner.nextInt();
+        Random rnd = new Random();
+        int computersNum = rnd.nextInt(3) + 1;
+        System.out.println("You chose: " + userNum);
+        System.out.println("The computer chose: " + computersNum);
+        numberScanner.close();
 
     }
 }
